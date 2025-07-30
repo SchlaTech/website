34de86e9553c04af2f33e8dfe8d36910526111ce
Matthew Schlabach <69648781+Schlabach@users.noreply.github.com>
2025-07-30 13:23:33 -0400

    Fixed icon

--- src/app/layout.js
+++ src/app/layout.js
@@ -21,6 +21,26 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta
+        name="description"
+        content="Custom apps, training, and reporting for small businesses. Let SchlaTech help you modernize."
+      />
+
+      {/* Open Graph */}
+      <meta
+        property="og:title"
+        content="SchlaTech – Custom Software for Small Businesses"
+      />
+      <meta
+        property="og:description"
+        content="Custom apps, training, and reporting for small businesses. Let SchlaTech help you modernize."
+      />
+      <meta
+        property="og:image"
+        content="https://schlatech.com/images/icon.png"
+      />
+      <meta property="og:url" content="https://schlatech.com" />
+      <meta property="og:type" content="website" />
       <body className="antialiased bg-white text-gray-900">{children}</body>
     </html>
   );
