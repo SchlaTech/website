0c488966532f48dd3f08ebfbfb88e4032049b449
Matthew Schlabach <69648781+Schlabach@users.noreply.github.com>
2025-07-31 15:31:45 -0400

    update www fwd

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "www.schlatech.com",
+          },
+        ],
+        destination: "https://schlatech.com/:path*",
+        permanent: true,
+      },
+    ];
+  },
+};
+
 export default nextConfig;
