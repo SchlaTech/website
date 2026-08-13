75de7a54dca898f79eb182978e66709a3de337d5
Schlabach <matthewshawn89@gmail.com>
2026-08-12 23:08:57 -0400

    feature/updated-next.js

--- src/app/acumatica-development/page.jsx
+++ src/app/acumatica-development/page.jsx
@@ -2,6 +2,10 @@ import Link from "next/link";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
+// TODO: Cache Components adoption. Refactor this route so this opt-out can be removed.
+// See: https://nextjs.org/docs/app/guides/migrating-to-cache-components
+export const instant = false;
+
 const capabilities = [
   {
     title: "Custom Screens and Workflows",
